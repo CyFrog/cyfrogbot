7440657f5e2835d2d5217a1f159abd5f16049e13
dreamtrove <39628487+dreamtrove@users.noreply.github.com>
2019-12-26 19:03:34 -0500

    Update textHandler.js

--- src/handlers/textHandler.js
+++ src/handlers/textHandler.js
@@ -15,10 +15,11 @@ const privateChat = ctx => {
     Markup.keyboard([["/balance", "/help"],["/deposit", "/withdraw"]]).oneTime().resize().extra()); };
 
 const groupChat = async ctx => {
-
+  let dice=["🎲","⚀","⚁","⚂","⚃","⚄","⚅"]; 
   /// Listen for Tip Message from Group Chat  // RegEx "[number] cy";  // Example: "10 cy" , " 10cy" , "10 CyFrog";
   const re = /[0-9]+ *cyfrog/gi;  const reComma = /(\d{0,3},)?(\d{3},)?\d{0,3} *cyfrog/i;  const reDot = /\d*\.?\d* *cyfrog/gi;
   const reClown = /🐸/g;  const reCircus = /🦎/g; const reFlower = /🌺/g; const reDice = /🎲/g;
+  
   
   if (ctx.message.reply_to_message) 
   { let text = ctx.message.text;
