3e78fc70cd06ee3750e31af284d9969bc58fd536
dreamtrove <39628487+dreamtrove@users.noreply.github.com>
2019-12-27 00:14:27 -0500

    Update textHandler.js

--- src/handlers/textHandler.js
+++ src/handlers/textHandler.js
@@ -22,20 +22,20 @@ const groupChat = async ctx => {
   const reClown = /🐸/g;  const reCircus = /🦎/g; const reFlower = /🌺/g; const reDice = /🎲/g;
   
   
-  if (ctx.message.reply_to_message) 
-  { let text = ctx.message.text;
-    if (  parseFloat(text.match(reDot)) || parseFloat(text.match(reComma))) 
-    { text = text.includes(".") ? text.match(reDot)[0] : text.match(reComma)[0];
-      if (text.includes(".")) 
-      { let amount = parseFloat(text.replace(/cyfrog/g, ""));
+  if (ctx.message.reply_to_message) {
+    let text = ctx.message.text;
+    if (parseFloat(text.match(reDot)) || parseFloat(text.match(reComma))) {
+      text = text.includes(".") ? text.match(reDot)[0] : text.match(reComma)[0];
+      if (text.includes(".")) {
+        let amount = parseFloat(text.replace(/cyfrog/g, ""));
         const tipResult = await tip(ctx, amount); ctx.replyWithMarkdown(diceText,tipResult); }
-      else if (text.includes(",")) 
-      { let amount = text.replace(/,/g, ""); const tipResult = await tip(ctx, amount); ctx.replyWithMarkdown(tipResult); }
-      else if (text.match(re)) 
-      { let amount = ctx.message.text.match(re)[0].split(" ")[0];
+      else if (text.includes(",")) {
+        let amount = text.replace(/,/g, ""); const tipResult = await tip(ctx, amount); ctx.replyWithMarkdown(tipResult); }
+      else if (text.match(re)) {
+        let amount = ctx.message.text.match(re)[0].split(" ")[0];
         const tipResult = await tip(ctx, amount); ctx.replyWithMarkdown(tipResult); } } 
-    else if (text.match(reClown) || text.match(reCircus) || text.match(reFlower) || text.match(reDice)) 
-    { let amount = 0; 
+    else if (text.match(reClown) || text.match(reCircus) || text.match(reFlower) || text.match(reDice)) {
+      let amount = 0; 
       if (text.match(reClown)) { const matchArray = text.match(reClown);  amount += matchArray.length * 0.01; }
       if (text.match(reFlower)) { const matchArray = text.match(reFlower);  amount += matchArray.length * 0.1; }
       if (text.match(reCircus)){ const matchArray = text.match(reCircus); amount += matchArray.length * 1; }
@@ -62,14 +62,14 @@ const tip = async (ctx, amount) => {
   await sessionInit(ctx);
 
   // Tip to bot deprecated
-  if (toUser.is_bot) 
-  { if (fromUser.id !== toUser.id) toggleLock(ctx, toUser.id); toggleLock(ctx, fromUser.id);
+  if (toUser.is_bot) {
+    if (fromUser.id !== toUser.id) toggleLock(ctx, toUser.id); toggleLock(ctx, fromUser.id);
     return `*${fromUser.first_name}* you can't tip to bot`; }
   const transactionSuccess = await transactionInit(amount, ctx, toUser);
   if (fromUser.id !== toUser.id) toggleLock(ctx, toUser.id); toggleLock(ctx, fromUser.id);
   let msg = "";
-  if (transactionSuccess) 
-  { msg += `*${fromUser.first_name}* tipped ${amount.toLocaleString("en-US")} 🐸*CyFrog*🐸 to *${toUser.first_name}*`; }
-  else 
-  { console.log("Need more CyFrog"); msg += `*${fromUser.first_name}* you need more 🐸*CyFrog*🐸`; } 
+  if (transactionSuccess) {
+    msg += `*${fromUser.first_name}* tipped ${amount.toLocaleString("en-US")} 🐸*CyFrog*🐸 to *${toUser.first_name}*`; }
+  else {
+    console.log("Need more CyFrog"); msg += `*${fromUser.first_name}* you need more 🐸*CyFrog*🐸`; } 
   return msg; };
