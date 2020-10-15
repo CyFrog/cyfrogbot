af7547571588261c879a86db2f414d2a735f2803
CyFrog <72286790+CyFrog@users.noreply.github.com>
2020-10-14 20:37:36 -0400

    Update commandHandler.js

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,14 +1,14 @@
-const Markup = require("telegraf/markup");
-const { sessionInit } = require("../sessionInit");
-const { deposit } = require("./commands/deposit");
-const { balance } = require("./commands/balance");
-const { withdraw } = require("./commands/withdraw");
-// const { checkDeposits } = require("./commands/checkDeposits");
+// const Markup = require("telegraf/markup");
+// const { sessionInit } = require("../sessionInit");
+// const { deposit } = require("./commands/deposit");
+// const { balance } = require("./commands/balance");
+// const { withdraw } = require("./commands/withdraw");
+// // const { checkDeposits } = require("./commands/checkDeposits");
 
 
 module.exports.commandHandler = bot => {
   bot.start(async ctx => {
-    if (ctx.chat.type == "private") await start(ctx);
+//    if (ctx.chat.type == "private") await start(ctx);
   });
 
   bot.help(ctx => {
@@ -20,74 +20,74 @@ module.exports.commandHandler = bot => {
   });
 
   bot.command("balance", async ctx => {
-    await isAllowed(ctx, balance);
+//    await isAllowed(ctx, balance);
   });
 
   bot.command("deposit", async ctx => {
-    await isAllowed(ctx, deposit);
+//    await isAllowed(ctx, deposit);
   });
 
   bot.command("withdraw", async ctx => {
-    await isAllowed(ctx, withdraw);
+//    await isAllowed(ctx, withdraw);
   });
 };
 
-const isAllowed = async (ctx, commandFunction) => {
-  if (ctx.chat.type == "private") {
-    if (ctx.from.is_bot) return ctx.reply('Only humans accepted.');
-    if (!ctx.session.wallet) await sessionInit(ctx);
-    await commandFunction(ctx)
-  }
-}
+// const isAllowed = async (ctx, commandFunction) => {
+//  if (ctx.chat.type == "private") {
+//    if (ctx.from.is_bot) return ctx.reply('Only humans accepted.');
+//    if (!ctx.session.wallet) await sessionInit(ctx);
+//    await commandFunction(ctx)
+//  }
+//}
 
-const start = async ctx => {
-  await sessionInit(ctx);
+// const start = async ctx => {
+//  await sessionInit(ctx);
 
-  ctx.reply(
-    `Hello ${ctx.from.first_name}!`,
-    Markup.keyboard([["/balance", "/help"], ["/deposit", "/withdraw"]])
-      .oneTime()
-      .resize()
-      .extra()
-  );
-};
+//  ctx.reply(
+//    `Hello ${ctx.from.first_name}!`,
+//    Markup.keyboard([["/balance", "/help"], ["/deposit", "/withdraw"]])
+//      .oneTime()
+//      .resize()
+//      .extra()
+//  );
+// };
 
-const help = ctx => {
-  helpMsg = `
-🐸*CyFrog HELP PAGE*🐸\n
+// const help = ctx => {
+//  helpMsg = `
+//🐸*CyFrog HELP PAGE*🐸\n
 
-To tip just type a number and cyfrog 
-eg. "0.01 cyfrog" when replying to a message 
-or 🐸=0.01 or 🎲=0.01x1-6 or 🌺=0.10 or 🦎=1
+// To tip just type a number and cyfrog 
+// eg. "0.01 cyfrog" when replying to a message 
+// or 🐸=0.01 or 🎲=0.01x1-6 or 🌺=0.10 or 🦎=1
 
-What can I help you with? 
+// What can I help you with? 
 
-Here are a list of my commands:
+// Here are a list of my commands:
 
-Type:
+// Type:
 
-/deposit - for information on depositing 
-/help - for information on tipping *CyFrog* 
-/withdraw - withdrawing *CyFrog*  
-/balance - get your *CyFrog* balance
+// /deposit - for information on depositing 
+// /help - for information on tipping *CyFrog* 
+// /withdraw - withdrawing *CyFrog*  
+// /balance - get your *CyFrog* balance
 
-If you need further assistance, please contact @dreamtrove
-`
-  ctx.replyWithMarkdown(
-    helpMsg,
-    Markup.keyboard([["/balance", "/help"], ["/deposit", "/withdraw"]])
-      .oneTime()
-      .resize()
-      .extra()
-  );
-};
+// If you need further assistance, please contact @dreamtrove
+// `
+//  ctx.replyWithMarkdown(
+//    helpMsg,
+//    Markup.keyboard([["/balance", "/help"], ["/deposit", "/withdraw"]])
+//      .oneTime()
+//      .resize()
+//      .extra()
+//  );
+// };
 
-const menu = ctx => {
-  ctx.reply(
-    `Main Menu:`,
-    Markup.keyboard([["/balance", "/help"], ["/deposit", "/withdraw"]])
-      .oneTime()
-      .resize()
-      .extra()
-  );
-};
+// const menu = ctx => {
+//  ctx.reply(
+//    `Main Menu:`,
+//    Markup.keyboard([["/balance", "/help"], ["/deposit", "/withdraw"]])
+//      .oneTime()
+//      .resize()
+//      .extra()
+//  );
+// };
