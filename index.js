a65a20221b23266675e325649af8143f6dfb942d
CyFrog <72286790+CyFrog@users.noreply.github.com>
2020-10-14 20:41:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const session = require("telegraf/session");
 const { textHandler } = require("./src/handlers/textHandler");
 const { commandHandler } = require("./src/handlers/commandHandler");
 const commandParts = require("telegraf-command-parts");
-// const { notification } = require("./src/notification");
+const { notification } = require("./src/notification");
 // const rateLimit = require("telegraf-ratelimit");
 
 // const bot = new Telegraf(process.env.BOT_TOKEN);
