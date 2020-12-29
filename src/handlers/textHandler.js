38c5c926179310b7bf76d425a94d67deaa805fdc
CyFrog <72286790+CyFrog@users.noreply.github.com>
2020-12-28 20:08:32 -0500

    Update textHandler.js

--- src/handlers/textHandler.js
+++ src/handlers/textHandler.js
@@ -1,4 +1,4 @@
-// const Markup = require("telegraf/markup");
+const Markup = require("telegraf/markup");
 // const { getSession } = require("../dynamoDB");
 // const { sessionInit } = require("../sessionInit");
 // const { transactionInit } = require("../transactionInit");
