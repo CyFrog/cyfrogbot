baab10c8629b7fafdba9bc2c4f8c62532147eebf
CyFrog <72286790+CyFrog@users.noreply.github.com>
2020-12-28 20:05:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Telegraf = require("telegraf");
 const session = require("telegraf/session");
-// const { textHandler } = require("./src/handlers/textHandler");
+const { textHandler } = require("./src/handlers/textHandler");
 // const { commandHandler } = require("./src/handlers/commandHandler");
 // const commandParts = require("telegraf-command-parts");
 // const { notification } = require("./src/notification");
